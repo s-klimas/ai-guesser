f965c39a3c4442cc737e7e1cc6b60dbfe8337d2c
s-klimas <sebastianklimas@op.pl>
2025-08-21 19:46:32 +0200

    Initial commit

---
+++ src/main/java/pl/sebastianklimas/aiguesser/AiGuesserApplication.java
@@ -0,0 +1,13 @@
+package pl.sebastianklimas.aiguesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGuesserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGuesserApplication.class, args);
+    }
+
+}
