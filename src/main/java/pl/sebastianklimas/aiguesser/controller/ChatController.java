33365904b87c4ba6376116d63d1fb6967d019306
s-klimas <sebastianklimas@op.pl>
2025-08-28 11:27:36 +0200

    Changed /ask from GET to POST

--- src/main/java/pl/sebastianklimas/aiguesser/controller/ChatController.java
+++ src/main/java/pl/sebastianklimas/aiguesser/controller/ChatController.java
@@ -2,6 +2,7 @@ package pl.sebastianklimas.aiguesser.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sebastianklimas.aiguesser.model.Game;
@@ -17,7 +18,7 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-    @GetMapping("/ask")
+    @PostMapping("/ask")
     public ResponseEntity<Response> chat(@RequestBody GameRequest gr) {
         return ResponseEntity.ok(chatService.ask(gr.message(), gr.game()));
     }
